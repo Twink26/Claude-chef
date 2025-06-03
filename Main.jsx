3a090be801a8b532a5cae66084aad9acbfc152a1
Twink26 <startwinklerana@gmail.com>
2025-06-03 14:36:34 +0530

    commit

---
+++ Main.jsx
@@ -0,0 +1,17 @@
+export default function Main(){
+    return (
+        <main>
+            <form className="form">
+                <input 
+                type="text"
+                aria-label="Add ingredient"
+                placeholder="e.g oregano"
+            />
+            <button>
+                Add ingredient
+            </button>
+
+            </form>
+        </main>
+    )
+}
