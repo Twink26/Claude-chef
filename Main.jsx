059cfcd6ea7ae8b1a3546e371c34808d9387e2c8
Twink26 <startwinklerana@gmail.com>
2025-06-04 08:58:21 +0530

    l

--- Main.jsx
+++ Main.jsx
@@ -1,17 +1,35 @@
 export default function Main(){
+    const ingredients = ["chicken", "Oregano", "Tomatoes"]
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event){
+        event.preventDefault()
+        console.log("Form submitted!")
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient)
+        console.log(ingredients)
+    }
     return (
         <main>
-            <form className="form">
+            <form  onSubmit={handleSubmit} className="form">
                 <input 
                 type="text"
                 aria-label="Add ingredient"
                 placeholder="e.g oregano"
+                name="ingredient"event
             />
             <button>
                 Add ingredient
             </button>
 
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
